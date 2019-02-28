4f0c89c52ae2f6861dc421f3776baf1e44487b24
Neilson <ngamble@redventures.com>
2019-02-28 09:33:50 -0500

    initial commit.  adding dependencies and setting up node / express server

---
+++ server/routes/api/posts.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongodb = require('mongodb');
+const router = express.Router();
+
+// Get Posts
+router.get('/', (req, res) => {
+    res.send('Hello');
+})
+
+
+// Add Posts
+
+
+
+// Delete Posts
+
+
+module.exports = router;
