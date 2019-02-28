acd3c31e49f07adb21d12a3517b0dc41cc60f2ab
Neilson <ngamble@redventures.com>
2019-02-28 15:29:44 -0500

    setting up get / add / delete skeleton api calls

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,16 +3,34 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // Get Posts
-router.get('/', (req, res) => {
-    res.send('Hello');
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
 })
 
-
 // Add Posts
-
-
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+})
 
 // Delete Posts
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+})
 
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb://neilgamb123:gamble123@ds145694.mlab.com:45694/jazzfest-grids', {
+        useNewUrlParser: true
+    });
+
+    return client.db('jazzfest-grids').collection('posts');
+}
 
 module.exports = router;
