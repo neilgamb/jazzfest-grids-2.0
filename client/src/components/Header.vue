d20e418527c15d6e81156941a33e30dd599575a4
Neilson <ngamble@redventures.com>
2019-03-13 21:12:13 -0400

    working on header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,33 +1,37 @@
 <template>
   <v-toolbar dark fixed app>
-    <v-toolbar-title>
-      <div class="logo">
-        <span class="logo-jazzfest">Jazz
-          <br>Fest
-        </span>
-        <span class="logo-grids">GRIDS</span>
+    <v-toolbar-title class="titleContainer">
+      <div class="logoContainer">
+        <div class="logo">
+          <span class="logo-jazzfest">Jazz
+            <br>Fest
+          </span>
+          <span class="logo-grids">GRIDS</span>
+        </div>
       </div>
 
-      <carousel
-        :navigateTo="currentDay"
-        :per-page="1"
-        :mouse-drag="false"
-        :paginationEnabled="false"
-        :paginationPadding="2"
-        :paginationSize="6"
-        :scrollPerPage="true"
-        paginationActiveColor="#f0f0f0"
-        paginationColor="#666"
-        @pageChange="setCurrentDay"
-      >
-        <slide v-for="date in dates" :key="date.date.toString()" class="date-container">
-          <div class="day-of-week">{{ dayOfWeek(date.date) }}</div>
-          <div class="calendar-container">
-            <div class="month">{{ monthOfYear(date.date) }}</div>
-            <div class="day">{{ date.date.getDate() }}</div>
-          </div>
-        </slide>
-      </carousel>
+      <div class="carouselContainer">
+        <carousel
+          :navigateTo="currentDay"
+          :per-page="1"
+          :mouse-drag="false"
+          :paginationEnabled="false"
+          :paginationPadding="2"
+          :paginationSize="6"
+          :scrollPerPage="true"
+          paginationActiveColor="#f0f0f0"
+          paginationColor="#666"
+          @pageChange="setCurrentDay"
+        >
+          <slide v-for="date in dates" :key="date.date.toString()" class="date-container">
+            <div class="day-of-week">{{ dayOfWeek(date.date) }}</div>
+            <div class="calendar-container">
+              <div class="month">{{ monthOfYear(date.date) }}</div>
+              <div class="day">{{ date.date.getDate() }}</div>
+            </div>
+          </slide>
+        </carousel>
+      </div>
 
     </v-toolbar-title>
   </v-toolbar>
@@ -65,72 +69,72 @@ export default {
 @import "../scss/_mixins.scss";
 @import "../scss/variables.scss";
 
-.logo {
-  position: absolute;
-  top: 2px;
-  left: 3px;
-  height: 53px;
-  // background: $background-color;
-  // box-shadow: 16px -20px 16px 6px $background-color;
-  display: flex;
-  align-items: center;
-  padding: 0px 10px;
-  z-index: 1;
+.titleContainer {
+  height: 100%;
+  width: 100%;
+  border: 1px solid white;
 
-  .logo-jazzfest {
-    font-family: "Rock Salt", cursive;
-    font-size: 20px;
-    color: $primary;
-    line-height: 23px;
-    position: relative;
-    top: 2px;
-    right: 6px;
-    z-index: 2;
-    transform: rotate(-32deg);
-    text-shadow: 1px 1px $black;
-  }
 
-  .logo-grids {
-    font-family: "Monoton", cursive;
-    font-size: 35px;
-    left: -30px;
-    position: relative;
-    text-align: center;
-    color: lighten($secondary, 10%);
-    animation: glow 3s ease-in-out infinite alternate;
-  }
 }
 
-.date-container {
-  display: flex;
-  justify-content: flex-end;
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+//   .logoContainer {
+//     height: 100%;
+//     display: flex;
+//     align-items: center;
 
-  .day-of-week {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0px 10px;
-    font-size: 22px;
-    font-family: "Sedgwick Ave", cursive;
-  }
+//     .logo-jazzfest {
+//       font-family: "Rock Salt", cursive;
+//       color: $primary;
+//       font-size: 20px;
+//       line-height: 23px;
+//       text-shadow: 1px 1px $black;
+//       transform: rotate(-32deg);
+//       z-index: 2;
+//       position: absolute;
+//       left: 10px;
+//       top: 5px;
+//     }
 
-  .calendar-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-right: 10px;
+//     .logo-grids {
+//       font-family: "Monoton", cursive;
+//       font-size: 35px;
+//       color: lighten($secondary, 10%);
+//       animation: glow 3s ease-in-out infinite alternate;
+//       position: absolute;
+//       margin-left: 35px;
+//     }
+//   }
 
-    .month {
-      font-size: 10px;
-    }
+// .date-container {
+//   display: flex;
+//   justify-content: flex-end;
+//   font-family: "Avenir", Helvetica, Arial, sans-serif;
 
-    .day {
-      font-size: 20px;
-      line-height: 20px;
-    }
-  }
-}
+//   .day-of-week {
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     // padding: 0px 10px;
+//     font-size: 22px;
+//     font-family: "Sedgwick Ave", cursive;
+//   }
+
+//   .calendar-container {
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+//     align-items: center;
+//     // margin-right: 10px;
+
+//     .month {
+//       font-size: 10px;
+//     }
+
+//     .day {
+//       font-size: 20px;
+//       line-height: 20px;
+//     }
+//   }
+// }
 </style>
 
