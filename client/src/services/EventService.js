04814d375dd2fdc1ed1492db3f66e68254d5216b
Neilson <ngamble@redventures.com>
2019-03-15 14:24:20 -0400

    adding server side work for deploy

--- client/src/services/EventService.js
+++ client/src/services/EventService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/api/events/';
+const url = 'api/events/';
 
 class EventService {
 
--- server/index.js
+++ server/index.js
@@ -14,6 +14,15 @@ app.use('/api/events', events);
 const venues = require('./routes/api/venues');
 app.use('/api/venues', venues);
 
+// handle production
+if(process.env.NODE_ENV === 'production') {
+  // static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  // handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
