14599a72370daf29a6247591cf5480192dc1b2ea
Neilson <ngamble@redventures.com>
2019-03-21 17:57:58 -0400

    syncing

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
 // Middleware
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.json())
+app.use(cors())
 
-const events = require('./routes/api/events');
-app.use('/api/events', events);
+const events = require('./routes/api/events')
+app.use('/api/events', events)
 
-const venues = require('./routes/api/venues');
-app.use('/api/venues', venues);
+const venues = require('./routes/api/venues')
+app.use('/api/venues', venues)
 
 // handle production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // static folder
-  app.use(express.static(__dirname + '/public/'));
+  app.use(express.static(__dirname + '/public/'))
 
   // handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`))
--- server/routes/api/venues.js
+++ server/routes/api/venues.js
@@ -1,36 +1,36 @@
-const express = require('express');
-const mongodb = require('mongodb');
-const router = express.Router();
+const express = require('express')
+const mongodb = require('mongodb')
+const router = express.Router()
 
 // Get Venues
 router.get('/', async (req, res) => {
-    const venues = await loadVenuesCollection();
-    res.send(await venues.find({}).toArray());
+  const venues = await loadVenuesCollection()
+  res.send(await venues.find({}).toArray())
 })
 
 // Add Venue
 router.post('/', async (req, res) => {
-    const venues = await loadVenuesCollection();
-    await venues.insertOne({
-        venue: req.body.text,
-        createdAt: new Date()
-    });
-    res.status(201).send();
+  const venues = await loadVenuesCollection()
+  await venues.insertOne({
+    venue: req.body.text,
+    createdAt: new Date()
+  })
+  res.status(201).send()
 })
 
 // Delete Venue
 router.delete('/:id', async (req, res) => {
-    const venues = await loadVenuesCollection();
-    await venues.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
-    res.status(200).send();
+  const venues = await loadVenuesCollection()
+  await venues.deleteOne({ _id: new mongodb.ObjectID(req.params.id) })
+  res.status(200).send()
 })
 
 async function loadVenuesCollection() {
-    const client = await mongodb.MongoClient.connect('mongodb://neilgamb123:gamble123@ds145694.mlab.com:45694/jazzfest-grids', {
-        useNewUrlParser: true
-    });
+  const client = await mongodb.MongoClient.connect('mongodb://neilgamb123:gamble123@ds145694.mlab.com:45694/jazzfest-grids', {
+    useNewUrlParser: true
+  })
 
-    return client.db('jazzfest-grids').collection('venues');
+  return client.db('jazzfest-grids').collection('venues')
 }
 
-module.exports = router;
+module.exports = router
